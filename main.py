2a9408916ac6dd01f13eb053ac0664a6ef551082
Максим Хильчук <maksimlhilchuk@gmail.com>
2022-03-12 13:55:37 +0300

    Second task

--- main.py
+++ main.py
@@ -7,12 +7,29 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_from_students(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        self.grades = {}
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -23,21 +40,26 @@ class Mentor:
         else:
             return 'Ошибка'
 
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 8)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
 
-class Reviewer(Mentor):
-    pass
-
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        self.grades = {}
+
+cool_lecturer = Lecturer('Another', 'One')
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_from_students(cool_lecturer, 'Python', 8)
+best_student.rate_from_students(cool_lecturer, 'Python', 10)
+best_student.rate_from_students(cool_lecturer, 'Python', 7)
+
+print(cool_lecturer.grades)
